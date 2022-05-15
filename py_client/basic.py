1cdc7f6d99fdcb7b1d5bb9369e3109c66228e8bf
Varun Mittal <vmittal24@gmail.com>
2022-05-15 16:18:35 +0530

    updated client to handle new enpoint /api

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,11 +1,11 @@
 import requests
 
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api"
 
 get_response = requests.get(endpoint)
 
-# print(get_response.text)
+print(get_response.text)
 
 print(get_response.json())
 
