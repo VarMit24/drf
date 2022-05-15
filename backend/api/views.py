f90a5a8ad495325b32646cdc60e82c88f40c5071
Varun Mittal <vmittal24@gmail.com>
2022-05-15 17:08:04 +0530

    updated api view to access product model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,9 @@
 import json
 from django.http import JsonResponse
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
+    model_data = Product.objects.all().order_by("?").first()
     print(dict(request.GET))
     body = request.body
     data = {}
@@ -10,6 +12,11 @@ def api_home(request, *args, **kwargs):
     except:
         pass
     print(data)
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     data['params'] = dict(request.GET)
     data['headers'] = dict(request.headers)
     data['content_type'] = request.content_type
