1ea64a0666f5e057e1eccbdfc37fbcda705ff5ab
Varun Mittal <vmittal24@gmail.com>
2022-05-15 16:35:55 +0530

    updated view to handle data and param passed into request

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,16 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response"})
+    print(dict(request.GET))
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
