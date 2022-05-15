a359bf450537d5ed29994d7b8233185f45b76cd6
Varun Mittal <vmittal24@gmail.com>
2022-05-15 16:25:02 +0530

    updated client to send param and json object

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api"
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"})
 
 print(get_response.text)
 
